df324fb512af330c6d841cd2416ff22c5b052d7c
narlo <aks152312@gmail.com>
2023-02-02 18:02:36 +0900

    typescript로 todo 구현하기

---
+++ typescript-todo/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from '@emotion/styled'
+import TaskProvider from './contexts/TaskProvider';
+import Header from './components/Header';
+import NewTaskForm from './components/NewTaskForm';
+import TaskList from './components/TaskList';
+const Container = styled.div`
+  width: 400px;
+  margin: 0 auto;
+`
+const App = () => {
+  return (
+    <TaskProvider>
+      <Container>
+        <Header>ToDo</Header>
+        <NewTaskForm />
+        <TaskList css={{marginTop: 16}}/>
+      </Container>
+    </TaskProvider>
+  )
+}
+
+export default App;
